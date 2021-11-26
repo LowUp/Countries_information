0c53882e1f34e48da0f389fcb64c85bfad0a246a
Emereole Chinduji <credo05@hotmail.fr>
2021-11-26 18:30:28 +0100

    fixed spelling mistake

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ const About = () => {
 
         <h1>About</h1>
         
-        <p>Small ReactJS app that displays general information about certain such as the country name, 
+        <p>Small ReactJS app that displays general information about countries such as the country name, 
         the capital and the population. This app also allows the user the filter countries by continent, 
         and to decide the number of countries displayed on the screen.
         </p>
